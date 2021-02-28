f463c6545fece09d5d0ac7abd8709236f9d95318
RodriguesRodrigo <rrscotti@hotmail.com>
2021-02-27 21:56:47 -0300

    changed body email from string to object

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -14,9 +14,9 @@ class SendMailController {
         const surveysRepository = getCustomRepository(SurveysRepository);
         const surveysUsersReposirory = getCustomRepository(SurveysUsersRepository);
 
-        const userAlreadyExists = await usersRepository.findOne({email});
+        const user = await usersRepository.findOne({email});
 
-        if (!userAlreadyExists) {
+        if (!user) {
             return response.status(400).json({
                 error: "User does not exists!",
             });
@@ -31,14 +31,20 @@ class SendMailController {
         }
 
         const surveyUser = surveysUsersReposirory.create({
-            user_id: userAlreadyExists.id,
+            user_id: user.id,
             survey_id,
         });
         await surveysUsersReposirory.save(surveyUser);
 
         const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
 
-        await SendMailService.execute(email, survey.title, survey.description, npsPath);
+        const variables = {
+            name: user.name,
+            title: survey.title,
+            description: survey.description,
+        }
+
+        await SendMailService.execute(email, survey.title, variables, npsPath);
         
         return response.status(200).json(surveyUser);
     }
